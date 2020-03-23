be6895c76f1138cbaf7cb14af5024354ff929dff
angelgus79 <agustavo79@gmail.com>
2020-03-23 09:49:49 -0700

    Starting the project and the Excercise 01

---
+++ XamarinCourse/XamarinCourse/XamarinCourse/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCourse
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BindingSliderPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
